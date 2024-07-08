b76e9b59c38c8bdf43664d4d475853529ee4040b
Dai Fujihara <daipresents@gmail.com>
2024-07-09 08:57:23 +0900

    Fixed 4.1

--- my-app/src/app/api/hello/route.ts
+++ my-app/src/app/api/hello/route.ts
@@ -1,4 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+
 // Route handler
 export async function GET(request: Request) {
   return new Response("Hello, Next.js!");
+}
+
+export async function POST(request: NextRequest) {
+  // ステータスコードの設定
+  // if (!session) {
+  //   retrun new NextResponse.json({message: "unauthorized"}, {status: 401});
+  // }
+  return NextResponse.json({ liked: true});
 }
