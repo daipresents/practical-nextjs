258441128daf7531ac1cbc37c6c02fdd1cd6245a
Dai Fujihara <daipresents@gmail.com>
2024-07-08 10:09:15 +0900

    Fixed 1-3

--- training-main/packages/training-web-1/src/app/categories/[categoryName]/page.tsx
+++ training-main/packages/training-web-1/src/app/categories/[categoryName]/page.tsx
@@ -1,3 +1,5 @@
+//'use client';
+
 type Props = {
   params: { categoryName: string };
   searchParams: { [key: string]: string | string[] | undefined };
@@ -6,11 +8,23 @@ type Props = {
 // ★:props からパスパラメーター、URL 検索パラメーターが参照できる
 export default function Page({ params, searchParams }: Props) {
   const page = typeof searchParams.page === "string" ? searchParams.page : "1";
+
+  //useRoute hookの使用。これでもナビゲーションを発火できる。
+  // 特に理由がなければLinkコンポーネントを使う
+  //const router = useRouter();
+
   return (
     <div>
       <h1>カテゴリー別一覧画面</h1>
       <h2>カテゴリー「{params.categoryName}」</h2>
       <p>ページ番号：「{page}」</p>
+
+      {/* <button onClick={() => {
+        router.push("/categories");
+      }}>
+        カテゴリ一覧に戻る
+      </button>
+      <button onClick={() => router.back()}>戻る</button> */}
     </div>
   );
 }
